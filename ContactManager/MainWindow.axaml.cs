ea7b69e3f2cba33525f1c597eec6f6faf622813b
Grzegorz <grzegorzgejlo@cyfroweszkoly.pl>
2024-10-28 15:50:50 +0100

    Utworzenie interfejsu w MainWindow.axaml za pomocą stackpanel, textbox, textblock oraz button do dodawania nowych kontaktów:

--- ContactManager/MainWindow.axaml.cs
+++ ContactManager/MainWindow.axaml.cs
@@ -1,4 +1,5 @@
 using Avalonia.Controls;
+using Avalonia.Interactivity;
 
 namespace ContactManager;
 
@@ -7,5 +8,10 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+    }
+
+    private void AddItem_Click(object? sender, RoutedEventArgs e)
+    {
+        throw new System.NotImplementedException();
     }
 }
--- ContactManager/obj/Debug/net8.0/ContactManager.AssemblyInfo.cs
+++ ContactManager/obj/Debug/net8.0/ContactManager.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ContactManager")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+604f77abc22c3ee26f67f950dd2e980ebc030595")]
 [assembly: System.Reflection.AssemblyProductAttribute("ContactManager")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ContactManager")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
